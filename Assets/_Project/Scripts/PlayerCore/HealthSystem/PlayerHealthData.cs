1808c268b000cbb98af361d1a61829686c86f8f8
Artem <gmcorp00x@mail.ru>
2024-02-21 20:22:48 +0300

    -Added game terrain with landscaping. -Added player's weapon model. -Added natural environment on the game scene. -Changed skyboxes.

---
+++ Assets/_Project/Scripts/PlayerCore/HealthSystem/PlayerHealthData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace PlayerCore.HealthSystem
+{
+    [CreateAssetMenu(fileName = "PlayerHealthData", menuName = "PlayerCoreMechanics/HealthData")]
+    public class PlayerHealthData : ScriptableObject
+    {
+        private float _playerHp;
+        private bool _isPlayerDead;
+
+        public float PlayerHp
+        {
+            get => _playerHp;
+            set => _playerHp = value;
+        }
+
+        public bool IsPlayerDead
+        {
+            get => _isPlayerDead;
+            set => _isPlayerDead = value;
+        }
+
+        public void ResetData()
+        {
+            _playerHp = 0f; 
+            _isPlayerDead = false;
+        }
+    }
+}
